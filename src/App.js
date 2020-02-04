0e33bc238cab4ae62817f78c69139c4e742a3bf3
Ryan McDonagh <ryan@henschen.com>
2020-02-04 16:55:58 -0500

    added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
+import ExpenseDashboardPage from './components/ExpenseDashboardPage/ExpenseDashboardPage.component';
+import AddExpensePage from './components/ExpenseDashboardPage/ExpenseDashboardPage.component';
+
+
+import 'normalize.css/normalize.css';
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            New
-        </div>
-    );
+class App extends React.Component {
+
+
+    render() {
+        return (
+            <Switch>
+                <div>
+                    <Route path="/" component={ExpenseDashboardPage} />
+                    <Route path="/create" component={AddExpensePage} />
+                </div>
+            </Switch>
+        );
+    }
+
 }
 
 export default App;
